ad94c8ff48d48ba4f5b72af2271ba5eed9601902
Agos Noriega <agosnoriega2018@gmail.com>
2022-10-31 20:32:21 -0300

    Arreglando el link de conexion

--- AgosN/src/main/java/com/MyPortfolio/AgosN/Controller/PersonaController.java
+++ AgosN/src/main/java/com/MyPortfolio/AgosN/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins={"https://myfront-2f457.web.app","http://localhost:4200"})
+@CrossOrigin(origins="https://myfront-2f457.web.app")
 public class PersonaController {
 
    @Autowired
